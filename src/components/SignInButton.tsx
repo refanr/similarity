49bd3cde98e25a5bfb8d3d7d55b8dc255ddbfa14
Reynir Gunnarsson <reynirg20@ru.is>
2023-05-19 22:41:31 +0000

    tailwind, theme, toast

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -3,6 +3,7 @@
 import { FC, useState } from "react";
 import Button from "@/ui/Button";
 import { signIn } from "next-auth/react";
+import { toast } from "@/ui/Toast";
 
 interface SignInButtonProps {}
 
@@ -13,20 +14,19 @@ const SignInButton: FC<SignInButtonProps> = ({}) => {
     setIsLoading(true);
 
     try {
-      await signIn("google");
-    } catch (error) {
-      //   toast({
-      //     title: "Error signing in",
-      //     message: "Please try again later",
-      //     type: "error",
-      //   });
-    }
+      // await signIn("google");
+      toast({
+        title: "Error signing in",
+        message: "Please try again later",
+        type: "error",
+      });
+    } catch (error) {}
   };
   return (
     <>
-    <Button onClick={signInWithGoogle} isLoading={isLoading}>
-      Sign in
-    </Button>
+      <Button onClick={signInWithGoogle} isLoading={isLoading}>
+        Sign in
+      </Button>
     </>
   );
 };
